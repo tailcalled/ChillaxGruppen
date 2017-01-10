672f0d9b72711d2e36cc0af7c16a72520f58ec4a
tailcalled <tailcalled@gmail.com>
2017-01-10 15:54:18 +0100

    Finished DMA 11, started PoP 11

---
+++ PoP/g11/Geom.fs
@@ -0,0 +1,20 @@
+module Geom
+
+type V3(x, y, z: float) = class
+   
+   member this.X = x
+   member this.Y = y
+   member this.Z = z
+
+   static member (+) (v1: V3, v2: V3) = V3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z)
+   static member (*) (v1: V3, s: float) = V3(v1.X * s, v1.Y * s, v1.Z * s)
+   static member (/) (v1: V3, s: float) = V3(v1.X / s, v1.Y / s, v1.Z / s)
+   static member (.*) (v1: V3, v2: V3) = v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z
+   static member (~-) (v: V3) = v * (-1.0)
+   static member (-) (v1: V3, v2: V3) = v1 + (-v2)
+
+   member this.Norm2 = this*this
+   member this.Norm = sqrt this.Norm2
+   member this.Unit = this / this.Norm
+
+end
